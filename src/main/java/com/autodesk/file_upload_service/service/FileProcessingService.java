eb4bb0ef242915db12c999cb4a08d34d455127ea
IDRIS NEEZAM <idrisneezam.2021@smu.edu.sg>
2025-09-19 02:18:41 +0800

    define FileProcessingService interface with method signatures

--- src/main/java/com/autodesk/file_upload_service/service/FileProcessingService.java
+++ src/main/java/com/autodesk/file_upload_service/service/FileProcessingService.java
@@ -1,23 +1,28 @@
 package com.autodesk.file_upload_service.service;
 
 import com.autodesk.file_upload_service.domain.ProcessedFile;
-import org.springframework.stereotype.Service;
 
-@Service
-public class FileProcessingService {
+public interface FileProcessingService {
     
-    public ProcessedFile processFile(String fileContent, String filename) {
-        // TODO: implement file processing logic
-        return null;
-    }
+    /**
+     * Processes a file and returns the processed result
+     * @param fileContent the content of the file to process
+     * @param filename the name of the file
+     * @return ProcessedFile containing the processing results
+     */
+    ProcessedFile processFile(String fileContent, String filename);
     
-    public long countLines(String content) {
-        // TODO: implement line counting logic
-        return 0;
-    }
+    /**
+     * Counts the number of lines in the given content
+     * @param content the content to count lines in
+     * @return the number of lines
+     */
+    long countLines(String content);
     
-    public long countWords(String content) {
-        // TODO: implement word counting logic
-        return 0;
-    }
+    /**
+     * Counts the number of words in the given content
+     * @param content the content to count words in
+     * @return the number of words
+     */
+    long countWords(String content);
 }
